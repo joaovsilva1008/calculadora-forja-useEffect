7702404ad2ad1fc3d4e4936074a85a0db9d59711
jvzin <joao.v.silva1008@aluno.senai.br>
2025-11-13 16:00:08 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import styles from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [n1, setN1] = useState()
+  const [n2, setN2] = useState()
+  const [respSoma, setRespSoma] = useState()
+  const [respSub, setRespSub] = useState()
+  const [respMulti, setRespMulti] = useState()
+  const [respDiv, setRespDiv] = useState()
 
+
+  useEffect(() =>{
+    if(n1 !== undefined && n2 !== undefined && !isNaN(n1) && !isNaN(n2)){
+    setRespSoma(parseFloat(n1) + parseFloat(n2))
+    setRespSub(parseFloat(n1) - parseFloat(n2))
+    setRespMulti(parseFloat(n1) * parseFloat(n2))
+    setRespDiv(parseFloat(n1) / parseFloat(n2))
+    }
+  }, [n1, n2])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={styles.wrapAll}>
+     <h1 className={styles.title}>Calculadora</h1>
+     <p>Digite números válidos para calcular</p>
+     <div className={styles.wrap}>
+      <input type="number" onChange={(e) => setN1(e.target.value)} value={n1} placeholder='Insira um número'/>
+      <input type="number" onChange={(e) => setN2(e.target.value)} value={n2} placeholder='Insira outro número'/>
+
+     </div>
+
+     <div className={styles.wrapResps}>
+      <h2>Resultado da soma</h2>
+      <h3>{respSoma}</h3>
+
+     </div>
+    <div className={styles.wrapResps}>
+      <h2>Resultado da subtração</h2>
+      <h3>{respSub}</h3>
+
+    </div>
+    <div className={styles.wrapResps}>
+      <h2>Resultado da multiplicação</h2>
+      <h3>{respMulti}</h3>
+
+    </div>
+    <div className={styles.wrapResps}>
+      <h2>Resultado da divisão</h2>
+      <h3>{respDiv}</h3>
+
+    </div>
+    </div>
   )
 }
 
